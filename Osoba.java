381506803cd90159b00d576e5f8cf3625dc745ca
VK <VK@DESKTOP-DMLBATG>
2023-03-28 09:38:08 +0200

    Init commit

---
+++ Osoba.java
@@ -0,0 +1,9 @@
+package ata;
+
+public class Osoba {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Testiranje instalacije je uspjesno");
+	}
+}
